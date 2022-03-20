62aa6f0ca0f3b1bb0e8c6fd78929e7dc861e1a1b
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-20 07:51:26 -0300

    Usando o list title

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,16 @@ class _MyHomePageState extends State<MyHomePage> {
         title: 'Conta de Luz',
         value: 211.30,
         date: DateTime.now().subtract(const Duration(days: 4))),
+    Transaction(
+        id: 't3',
+        title: 'Cartão de crédito',
+        value: 100211.30,
+        date: DateTime.now()),
+    Transaction(
+        id: 't4',
+        title: 'Lanche',
+        value: 11.30,
+        date: DateTime.now().subtract(const Duration(days: 4))),
   ];
 
   List<Transaction> get _recentTransactions {
