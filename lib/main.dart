17f50b3af72b8e4c04da75bdf5aada6682aed173
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-22 07:26:24 -0300

    Objeto Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -93,14 +93,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text(
         "Despesas Pessoais",
         style: TextStyle(
-          fontSize: 20 * MediaQuery.of(context).textScaleFactor,
+          fontSize: 20 * mediaQuery.textScaleFactor,
         ),
       ),
       actions: <Widget>[
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availableHeigth = MediaQuery.of(context).size.height -
+    final availableHeigth = mediaQuery.size.height -
         appBar.preferredSize.height -
         MediaQuery.of(context).padding.top;
 
