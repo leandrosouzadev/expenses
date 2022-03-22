ff640771fa55b3d2e390f3fc5288af4d746a3177
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-22 06:28:39 -0300

    Modo paisagem #01

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -116,14 +117,29 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(
-              height: availableHeigth * 0.25,
-              child: Chart(_recentTransactions),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Exibir Gráfico'),
+                Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    }),
+              ],
             ),
-            SizedBox(
-              height: availableHeigth * 0.75,
-              child: TransactionList(_transactions, _removeTransaction),
-            ),
+            if (_showChart)
+              SizedBox(
+                height: availableHeigth * 0.25,
+                child: Chart(_recentTransactions),
+              ),
+            if (!_showChart)
+              SizedBox(
+                height: availableHeigth * 0.75,
+                child: TransactionList(_transactions, _removeTransaction),
+              ),
           ],
         ),
       ),
